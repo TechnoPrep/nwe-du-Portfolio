04d986ca894eb803211ce819b5cc4c8e86964af6
nwe <nathan.ehrlich2621@gmail.com>
2021-07-09 15:21:59 -0600

    Add: Added JQuery so that when a Nav-link is clicked, it hides the Hamburger menu

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,9 +4,14 @@ $(document).ready(function(){
         $('.nav-menu').toggleClass('open');
         $('.fa-bars').toggleClass('open');
         $('.fa-minus-square').toggleClass('open');
-        if($('.nav-bg').css('visibility')==='hidden'){
-            $('.nav-bg').css('visibility','visible');
-        } else $('.nav-bg').css('visibility','hidden');
     });
 });
 
+$(document).ready(function(){
+    $('.nav-link').on('click', function(){
+        $('.nav-icon').toggleClass('open');
+        $('.nav-menu').toggleClass('open');
+        $('.fa-bars').toggleClass('open');
+        $('.fa-minus-square').toggleClass('open');
+    });
+});
